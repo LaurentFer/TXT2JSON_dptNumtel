742f004a1426e827098a76da6a40254a5f12d2f1
LaurentFer <laurent.ferrer@me.com>
2016-08-28 16:30:26 +0200

    Ajout du cas ou la ligne dpt existe mais ne contient pas le numéro de téléphone

--- src/main/java/AnalyseLigne.java
+++ src/main/java/AnalyseLigne.java
@@ -1,10 +1,14 @@
-
 public class AnalyseLigne {
 
 	public static String AnalyseCetteLigne(String ligneAanalyser){
 		
+		CharSequence numTel = "erreur";	//on initialise la variable sur "erreur" au cas ou la ligne ne comporterait pas de numéro de téléphone
+		
 		int positionNumTel 	= ligneAanalyser.substring(3).indexOf("0");			//On cherche la position du 0 (début numero tel) a partir de la sous chaine excluant le dpt
-		CharSequence numTel = ligneAanalyser.substring(3).subSequence(positionNumTel,positionNumTel+10);	//On définit une variable qui prend la séquence des 10char a partir de la position précédente
+		if (positionNumTel!=-1){
+			numTel = ligneAanalyser.substring(3).subSequence(positionNumTel,positionNumTel+10);	//On définit une variable qui prend la séquence des 10char a partir de la position précédente
+		} 
+		
 		return numTel.toString();
 	}
 }
